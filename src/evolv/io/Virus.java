3a25bda8ffc3e361d6a5af6ed643ff03dbe43178
Arun S. Johnson <arun2642@gmail.com>
2019-02-14 15:01:06 -0800

    Added everything.

---
+++ src/evolv/io/Virus.java
@@ -0,0 +1,10 @@
+package evolv.io;
+import java.util.Random;
+
+public class Virus{
+	private int brainSize = (Configuration.NUM_EYES * 3 + Configuration.MEMORY_COUNT + 6) * Configuration.BRAIN_WIDTH;
+	public int targetReceptor = (int)Math.random() * brainSize + 1;
+	public double receptorPreference = Math.random();
+	public double dangerLevel = 0.1;
+	private boolean exists = true;
+}
